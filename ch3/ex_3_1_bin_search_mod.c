972e9508551808e6bb1e39883ba29a0c278ca2c0
Guy Or <guydude1@gmail.com>
2022-10-14 10:14:18 +0300

    sol ex 3-1

--- ch3/ex_3_1_bin_search_mod.c
+++ ch3/ex_3_1_bin_search_mod.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-int binsearch(int x, int v[], int n);
+int binsearch_mod(int x, int v[], int n);
+
+int binsearch_mod(int x, int v[], int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n;
+
+    while (low < high)
+    {
+        mid = low + ((high - low) / 2);
+        if (x > v[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    return (low < n && v[low] == x) ? low : -1;
+}
